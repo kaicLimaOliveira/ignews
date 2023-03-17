30ff3a251d099ac23bf552f16f49e82f0f4af6ac
kaicLimaOliveira <kaic10@outlook.com>
2023-03-16 22:59:27 -0300

    first commit

---
+++ src/services/stripe.ts
@@ -0,0 +1,19 @@
+import Stripe from "stripe";
+import { version } from "../../package.json"
+
+const stripeApiKey = process.env.STRIPE_API_KEY;
+
+if (!stripeApiKey) {
+  throw new Error("Stripe API key not found");
+}
+
+export const stripe = new Stripe(
+  stripeApiKey,
+  {
+    apiVersion: "2022-11-15",
+    appInfo: {
+      name: "Ignews",
+      version
+    }
+  }
+);
